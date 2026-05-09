85f4cb83e114d520ad779a65b4929bb6183844b7
qianweiran <54937430+qianweiran@users.noreply.github.com>
2026-05-09 10:55:44 +0800

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -1,9 +1,26 @@
-from Varpheno import run_snp_celltype_score
+from VarPheno import prepare_feature_matrix
 
-res = run_snp_celltype_score(
+prepare_feature_matrix(
+    snv_sample_file="ExampleData/1-SNV_SampleCellType_Matrix.txt",
+    sample_cell_file="ExampleData/2-SampleCelltype_Cell.txt",
+    peaks_cell_file="ExampleData/3-Peaks_Cell.pkl",
+    out_npz="ExampleData/Feature.npz"
+)
+
+from VarPheno import prepare_edge_matrix
+
+prepare_edge_matrix(
+    snv_cell_npz="ExampleData/Feature.npz",
+    snn_file="ExampleData/Snn_connectivities.npz",
+    out_npy="ExampleData/Edge.npy"
+)
+
+from VarPheno import run_snv_celltype_score
+
+run_snv_celltype_score(
     matrix_npz = "ExampleData/Feature.npz",
+    Edge_npy = "ExampleData/Edge.npy",
     label_csv = "ExampleData/Cell_label.csv",
-    similarity_npy = "ExampleData/Similarity_Snn.npy",
-    snp_name_file = "ExampleData/SNP.txt",
-    outdir="OutPut"
+    snv_name_file = "ExampleData/SNV.txt",
+    outdir="Output"
 )
